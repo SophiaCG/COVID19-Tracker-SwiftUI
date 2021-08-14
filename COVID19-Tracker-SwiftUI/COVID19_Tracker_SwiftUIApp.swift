d6fee71e2464bc1c9e738a8767bd4c2b9a6e1ede
SophiaCG <sophia.gorgonio@gmail.com>
2021-08-14 01:54:23 -0400

    Configured Firebase

--- COVID19-Tracker-SwiftUI/COVID19_Tracker_SwiftUIApp.swift
+++ COVID19-Tracker-SwiftUI/COVID19_Tracker_SwiftUIApp.swift
@@ -7,17 +7,28 @@
 
 import SwiftUI
 import Firebase
+import Charts
 
 @main
 struct COVID19_Tracker_SwiftUIApp: App {
     
     init() {
-        FirebaseApp.configure()
+        let filePath = Bundle.main.path(forResource: "GoogleService-Info-CTS", ofType: "plist")!
+        let options = FirebaseOptions(contentsOfFile: filePath)
+        FirebaseApp.configure(options: options!)
     }
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(entries: [
+                //x - position of a bar, y - height of a bar
+                BarChartDataEntry(x: 1, y: 1),
+                BarChartDataEntry(x: 2, y: 1),
+                BarChartDataEntry(x: 3, y: 1),
+                BarChartDataEntry(x: 4, y: 1),
+                BarChartDataEntry(x: 5, y: 1)
+
+            ])
         }
     }
 }
--- COVID19-Tracker-SwiftUI/Views/ContentView.swift
+++ COVID19-Tracker-SwiftUI/Views/ContentView.swift
@@ -8,23 +8,52 @@
 import SwiftUI
 import Charts
 
-struct ContentView: View {
-    
-    @State var country = ""
-    
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear() {
-                ViewModel().getResults { results in
-                    self.country = results[0].country
-                }
-            }
+struct ContentView : UIViewRepresentable {
+    //Bar chart accepts data as array of BarChartDataEntry objects
+    var entries : [BarChartDataEntry]
+    // this func is required to conform to UIViewRepresentable protocol
+    func makeUIView(context: Context) -> BarChartView {
+        //crate new chart
+        let chart = BarChartView()
+        //it is convenient to form chart data in a separate func
+        chart.data = addData()
+        return chart
     }
+    
+    // this func is required to conform to UIViewRepresentable protocol
+    func updateUIView(_ uiView: BarChartView, context: Context) {
+        //when data changes chartd.data update is required
+        uiView.data = addData()
+    }
+    
+    func addData() -> BarChartData{
+        let data = BarChartData()
+        //BarChartDataSet is an object that contains information about your data, styling and more
+        let dataSet = BarChartDataSet(entries: entries)
+        // change bars color to green
+        dataSet.colors = [NSUIColor.green]
+        //change data label
+        dataSet.label = "My Data"
+        data.append(dataSet)
+        return data
+    }
+    
+    typealias UIViewType = BarChartView
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+
+struct Bar_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(entries: [
+            //x - position of a bar, y - height of a bar
+            BarChartDataEntry(x: 1, y: 1),
+            BarChartDataEntry(x: 2, y: 1),
+            BarChartDataEntry(x: 3, y: 1),
+            BarChartDataEntry(x: 4, y: 1),
+            BarChartDataEntry(x: 5, y: 1)
+
+        ])
     }
 }
